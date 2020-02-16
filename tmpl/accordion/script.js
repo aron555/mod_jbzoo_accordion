41379528ed90c759eb9cf8a278eda62a184f6ed6
Ivan Aronov <aron555@inbox.ru>
2020-02-17 02:01:03 +0300

    Удалил лишний скрипт, внес изменения в скрипт аккордиона по высоте

--- tmpl/accordion/script.js
+++ tmpl/accordion/script.js
@@ -1,3 +1,6 @@
 jQuery(function ($) {
-
+    let containerWidth = $('.container').width(); // Ширина контейнера для дропдауна
+    $('.zoo-list').css("width", containerWidth); // Задаем ширину списку равной ширине родительского контейнера (не всей ширине экрана)
+    let windowHeight = $(window).height(); // Задаем высоту экрана
+    $('.zoo-list').css("height", (windowHeight/100*90)).css("overflow-y", "auto"); // И возможность прокручивать список
 });
